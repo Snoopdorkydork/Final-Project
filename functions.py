1bdb1ce4d906b6c25bb75dfe706ab3fd8d807c47
Snoopdorkydork <mahingadkari@gmail.com>
2021-12-16 17:36:13 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,28 +6,91 @@ given the weather conditions.
 '''
 
 
-class Fire_Predictor():
-    def __init__(self):
-        self.temp = input('Please enter maximum Temperature in Celcius : ')
-        self.RH = input(
+def get_inputs():
+    while True:
+        temp = input('Please enter Temperature in Celcius : ')
+        try:
+            temp = float(temp)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        RH = input(
             'Please enter Reletive Humidity (RH) percentage value : ')
-        self.wind = input('Please enter Wind speed in km/h : ')
-        self.rain = input('Please enter rain in mm :')
-        self.FFMC = input(
+        try:
+            RH = float(RH)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        wind = input('Please enter Wind speed in km/h : ')
+        try:
+            wind = float(wind)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        rain = input('Please enter rain in mm : ')
+        try:
+            rain = float(rain)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        FFMC = input(
             'Please enter Fine Fuel Moisture Code (FFMC) index : ')
-        self.DMC = input(
-            'Please enter Duff Moisture Code (DMC) index :')
-        self.DC = input(
-            'Please enter Drought Code (DC) index :')
-        self.ISI = input(
-            'Please enter Initial Spread Index (ISI) :')
-        self.BUI = input(
-            'Please enter Buildup Index (BUI) :')
-        self.FWI = input(
-            'Please enter Fire Weather Index (FWI) :')
+        try:
+            FFMC = float(FFMC)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        DMC = input(
+            'Please enter Duff Moisture Code (DMC) index : ')
+        try:
+            DMC = float(DMC)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        ISI = input(
+            'Please enter Initial Spread Index (ISI) : ')
+        try:
+            ISI = float(ISI)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        BUI = input(
+            'Please enter Buildup Index (BUI) : ')
+        try:
+            BUI = float(BUI)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        FWI = input(
+            'Please enter Fire Weather Index (FWI) : ')
+        try:
+            FWI = float(FWI)
+            break
+        except ValueError:
+            print("Enter a valid value!")
+    while True:
+        DC = input(
+            'Please enter Drought Code (DC) index : ')
+        try:
+            DC = float(DC)
+            break
+        except ValueError:
+            print("Enter a valid value!")
 
+    input_val = [temp, RH, wind, rain, FFMC, DMC, DC, ISI, BUI, FWI]
+    return input_val
 
 
 if __name__ == "__main__":
     print("***Welcome to the Forest Fire Predictor***")
     print("Enter the required metereological data as requested")
+    inputs = get_inputs()
+    print(inputs)
