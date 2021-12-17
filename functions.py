7f55acc2bba0d443effdae8c1aa75b053751fb01
Snoopdorkydork <mahingadkari@gmail.com>
2021-12-16 21:21:47 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,6 +14,7 @@ from sklearn.naive_bayes import GaussianNB
 import pickle
 import scipy
 import numpy as np
+import pyglet
 
 
 def get_inputs():
@@ -107,6 +108,30 @@ def get_model_type():
         else:
             print("Enter valid model code!")
     return model_type
+
+
+def display_output(prediction):
+    '''
+    '''
+    fire_file = "Output/spongebob.gif"
+    not_fire_file = "Output/diablo.gif"
+
+    fire_animation = pyglet.resource.animation(fire_file)
+    not_fire_animation = pyglet.resource.animation(not_fire_file)
+
+    if prediction[0] == 1:
+        sprite = pyglet.sprite.Sprite(fire_animation)
+        win = pyglet.window.Window(
+            width=sprite.width, height=sprite.height)
+        print("Prediction: FIRE")
+
+    elif prediction[0] == 0:
+        sprite = pyglet.sprite.Sprite(not_fire_animation)
+        win = pyglet.window.Window(
+            width=sprite.width, height=sprite.height)
+        print("Prediction: NO FIRE")
+
+    return win, sprite
 
 
 def Model_train(model_type, filepath='Data/forestfires.csv'):
@@ -311,51 +336,18 @@ def Model_predict(model_type, X):
 
 
 if __name__ == "__main__":
-    print("************         Welcome to the Forest Fire Predictor        ************")
-    print("This predictor works on various machine learning classification\n" +
-          "algorithms such as Decission Tree, Support Vector Machine, Multi Level Perceptron \n" +
-          "and Gaussian Naive Bayes.\n")
-    print("To select which alogrithm to use  for the prediction use the given legend and \n" +
-          "input the code when propted for model type:\n")
-    print("Model: Decision Tree Classifier                                    Code: DT\n" +
-          "Model: Support Vector Classifier                                   Code: SVC\n" +
-          "Model: Multi Level Perceptron Classifier                           Code: MLP\n" +
-          "Model: Gaussian Naive Bayes Classifier                             Code: GNB\n" +
-          "Model: The concensus between all models gives us the prediction    Code: ALL\n")
     # model_type = get_model_type()
+    # inputs = get_inputs()
     model_type = "DT"
     inputs = [[29.0, 57.0, 18.0, 0, 65.7, 3.4, 7.6, 1.3, 3.4, 0.5]]
-    # while True:
-    #     Train = input("Do you want to retrain the model?(Y/N) : ")
-    #     if Train == "Y":
-    #         Model_train(model_type)
-    #         break
-    #     elif Train == "N":
-    #         break
-    #     else:
-    #         print("Enter a valid answer!")
-    # while True:
-    #     Score = input("Do you want to know the accuracy of the model?(Y/N) : ")
-    #     if Score == "Y":
-    #         Model_score(model_type)
-    #         break
-    #     elif Score == "N":
-    #         break
-    #     else:
-    #         print("Enter a valid answer!")
-    # while True:
-    #     Predict = input("Do you want to make a prediction?(Y/N) : ")
-    #     if Score == "Y":
-    #         print("Enter the required data as requested")
-    #         # inputs = get_inputs()
-    #         prediction = Model_predict(model_type, X=inputs)
-    #         break
-    #     elif Score == "N":
-    #         break
-    #     else:
-    #         print("Enter a valid answer!")
-
     Model_score(model_type)
     prediction = Model_predict(model_type, X=inputs)
     print(prediction)
     # print(inputs)
+    win, sprite = display_output(prediction)
+
+    @win.event
+    def on_draw():
+        win.clear()
+        sprite.draw()
+    pyglet.app.run()
