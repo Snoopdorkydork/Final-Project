ac17df643a24c9ce5532724136e8089d828bcad9
Snoopdorkydork <mahingadkari@gmail.com>
2021-12-16 20:32:15 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,6 +13,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 import pickle
 import scipy
+import numpy as np
 
 
 def get_inputs():
@@ -94,8 +95,18 @@ def get_inputs():
         except ValueError:
             print("Enter a valid value!")
 
-    input_val = [[temp, RH, wind, rain, FFMC, DMC, DC, ISI, BUI, FWI]]
-    return input_val
+    in_val = np.array([[temp, RH, wind, rain, FFMC, DMC, DC, ISI, BUI, FWI]])
+    return in_val
+
+
+def get_model_type():
+    while True:
+        model_type = input("Please enter the model type: ")
+        if model_type in ["DT", "SVC", "MLP", "GNB", "ALL"]:
+            break
+        else:
+            print("Enter valid model code!")
+    return model_type
 
 
 def Model_train(model_type, filepath='Data/forestfires.csv'):
@@ -300,14 +311,51 @@ def Model_predict(model_type, X):
 
 
 if __name__ == "__main__":
-    print("***Welcome to the Forest Fire Predictor***")
-    print("Enter the required metereological data as requested")
+    print("************         Welcome to the Forest Fire Predictor        ************")
+    print("This predictor works on various machine learning classification\n" +
+          "algorithms such as Decission Tree, Support Vector Machine, Multi Level Perceptron \n" +
+          "and Gaussian Naive Bayes.\n")
+    print("To select which alogrithm to use  for the prediction use the given legend and \n" +
+          "input the code when propted for model type:\n")
+    print("Model: Decision Tree Classifier                                    Code: DT\n" +
+          "Model: Support Vector Classifier                                   Code: SVC\n" +
+          "Model: Multi Level Perceptron Classifier                           Code: MLP\n" +
+          "Model: Gaussian Naive Bayes Classifier                             Code: GNB\n" +
+          "Model: The concensus between all models gives us the prediction    Code: ALL\n")
+    # model_type = get_model_type()
     model_type = "DT"
     inputs = [[29.0, 57.0, 18.0, 0, 65.7, 3.4, 7.6, 1.3, 3.4, 0.5]]
-    # inputs = get_inputs()
-    Model_train(model_type)
+    # while True:
+    #     Train = input("Do you want to retrain the model?(Y/N) : ")
+    #     if Train == "Y":
+    #         Model_train(model_type)
+    #         break
+    #     elif Train == "N":
+    #         break
+    #     else:
+    #         print("Enter a valid answer!")
+    # while True:
+    #     Score = input("Do you want to know the accuracy of the model?(Y/N) : ")
+    #     if Score == "Y":
+    #         Model_score(model_type)
+    #         break
+    #     elif Score == "N":
+    #         break
+    #     else:
+    #         print("Enter a valid answer!")
+    # while True:
+    #     Predict = input("Do you want to make a prediction?(Y/N) : ")
+    #     if Score == "Y":
+    #         print("Enter the required data as requested")
+    #         # inputs = get_inputs()
+    #         prediction = Model_predict(model_type, X=inputs)
+    #         break
+    #     elif Score == "N":
+    #         break
+    #     else:
+    #         print("Enter a valid answer!")
+
     Model_score(model_type)
     prediction = Model_predict(model_type, X=inputs)
     print(prediction)
-    print(type(prediction))
     # print(inputs)
